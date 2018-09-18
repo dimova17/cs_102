fc73adf93929bf88be9c188d868a4e6a5df4a4a6
Adelaide <31939581+dimova17@users.noreply.github.com>
2018-09-18 16:41:27 +0200

    Update config.py

--- vk_api/config.py
+++ vk_api/config.py
@@ -1,13 +1,13 @@
 """domain = 'https://api.vk.com/method/'
 v = '5.53'
 vk_token = 'Tокен доступа для ВК'
-plotly_username = 'dimova17',#Имя пользователя Plot.ly
-plotly_api_key = 'JUdIJJK1taivPxuS7aNL'#Ключ доступа Plot.ly"""
+plotly_username = '***',#Имя пользователя Plot.ly
+plotly_api_key = '***'#Ключ доступа Plot.ly"""
 import requests
 
 domain = "https://api.vk.com/method"
-access_token = 'f3fd754ff3fd754ff3fd754f6df3a28432ff3fdf3fd754fa99d2c7d1e100c335b8dcbe3'
-user_id = '162546124'  # PUT USER ID HERE
+access_token = 'f3fd754ff3fd754ff3fd754f6df3a28432ff3fdf3fd758dgngfe3'
+user_id = '0000001'  # PUT USER ID HERE
 
 query_params = {
     'domain' : domain,
